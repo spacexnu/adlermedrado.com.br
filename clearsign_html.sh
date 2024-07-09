1ee58d23916c915ea7c6b12fb25e0fd8f0d8a032
Adler Medrado <adlermedrado@gmail.com>
2024-07-09 10:23:49 -0300

    Improve clearsign bash code and rebuild site

--- clearsign_html.sh
+++ clearsign_html.sh
@@ -2,29 +2,50 @@
 
 shopt -s globstar
 
+# Signs an HTML file by appending a PGP signature.
+# Args:
+#   $1: Path to the HTML file to sign.
 function sign_html {
-  html=$1
-  temp_dir=/tmp/pgp-html-$$
+  local html_file="$1"
+  local temp_dir
 
-  echo "Copying file $1 to $temp_dir in order to be signed"
-  (
+  temp_dir=$(mktemp -d /tmp/pgp-html-XXXXXX)
+  if [[ ! -d "$temp_dir" ]]; then
+    echo "Failed to create temporary directory" >&2
+    return 1
+  fi
+
+  echo "Copying file $html_file to $temp_dir in order to be signed"
+  {
     echo '-->'
-    cat "$html"
+    cat "$html_file"
     echo '<!--'
-  ) >$temp_dir
+  } > "$temp_dir/file.html"
 
-  echo "Signing $temp_dir and renaming to $html"
-  (
+  if [[ $? -ne 0 ]]; then
+    echo "Failed to prepare file for signing" >&2
+    rm -rf "$temp_dir"
+    return 1
+  fi
+
+  echo "Signing $temp_dir/file.html and renaming to $html_file"
+  {
     echo '<!--'
-    gpg --clearsign --output - $temp_dir
+    gpg --clearsign --output - "$temp_dir/file.html"
     echo '-->'
-  ) >"$html"
+  } > "$html_file"
 
-  echo "Cleaning the garbage..."
-  rm $temp_dir
+  if [[ $? -ne 0 ]]; then
+    echo "Failed to sign the file" >&2
+    rm -rf "$temp_dir"
+    return 1
+  fi
+
+  echo "Cleaning up temporary files..."
+  rm -rf "$temp_dir"
 }
 
-for i in public/**/*.html; do
-  sign_html "$i"
+# Process all HTML files in the public directory and its subdirectories.
+for html_file in public/**/*.html; do
+  sign_html "$html_file"
 done
-
